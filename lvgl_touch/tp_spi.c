e3f4456d68d122635674a89e8bef28c33876d818
J. Pereira <j.pereira@cop-amaco.com>
2024-06-17 16:56:49 +0200

    Adding kconfig fullduplex touchpad configuration

--- lvgl_touch/tp_spi.c
+++ lvgl_touch/tp_spi.c
@@ -59,9 +59,13 @@ void tp_spi_add_device(spi_host_device_t host)
 		.command_bits = 8,
 		.address_bits = 0,
 		.dummy_bits = 0,
+#if CONFIG_LV_TOUCH_DRIVER_FULLDUPLEX == 1
+		.flags = SPI_DEVICE_NO_DUMMY,
+#else
 		.flags = SPI_DEVICE_HALFDUPLEX | SPI_DEVICE_NO_DUMMY,
+#endif
 	};
-	
+
 	//Attach the Touch controller to the SPI bus
 	tp_spi_add_device_config(host, &devcfg);
 }
